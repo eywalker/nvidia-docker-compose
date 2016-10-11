1279275c4f5716c1c115ca9df21a1045891eec69
Edgar Y. Walker <edgar.walker@gmail.com>
2016-10-11 09:15:25 -0500

    Implement support for Jinja2 templating of docker-compose.yml

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='nvidia-docker-compose',
-      version='0.0.3',
+      version='0.0.4',
       description='GPU enabled docker-compose wrapper',
       url='https://github.com/eywalker/nvidia-docker-compose',
       author='Edgar Y. Walker',
