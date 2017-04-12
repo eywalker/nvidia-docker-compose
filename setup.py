8903362599a795e37d998ba92574ed947f028707
Edgar Y. Walker <edgar.walker@gmail.com>
2017-04-12 09:11:45 -0500

    Increment to v0.1.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='nvidia-docker-compose',
-      version='0.1.2',
+      version='0.1.3',
       description='GPU enabled docker-compose wrapper',
       url='https://github.com/eywalker/nvidia-docker-compose',
       author='Edgar Y. Walker',
